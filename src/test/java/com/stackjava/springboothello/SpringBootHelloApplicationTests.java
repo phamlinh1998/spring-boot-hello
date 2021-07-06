95b5dee8d779f65ac790132b8a73cdd2cafaf7f2
linh <phamlinh1998>
2021-07-06 14:01:57 +0700

    first commit

---
+++ src/test/java/com/stackjava/springboothello/SpringBootHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stackjava.springboothello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
